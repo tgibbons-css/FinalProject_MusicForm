d055cf6f3e4a55568dd1a0103d8ab3f4a46db5da
OverTheBeez <91337046+OverTheBeez@users.noreply.github.com>
2021-12-22 20:53:01 -0600

    Worked on getting a random number generator coded in.

--- JO_CIS3285_FinalProject_MusicForm/randomSongItem.cs
+++ JO_CIS3285_FinalProject_MusicForm/randomSongItem.cs
@@ -8,5 +8,13 @@ namespace JO_CIS3285_FinalProject_MusicForm
 {
     class randomSongItem
     {
+
+        private readonly Random random = new Random();
+
+        public int RandomNumber(int min, int max)
+        {
+            return random.Next(min, max);
+        }
+
     }
 }
